d7cd418122ecef6207410171180d4d8d6ba3e480
Khalid Mahmud <khalid.mahmud@valmetpartners.com>
2023-08-18 15:46:15 +0600

    first-commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace DOT_NET_7_Assignment_01_Swapno_SuperShop;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
